71abc66f3231f75d5bce8fe41911494ea4fc1bf7
juno-r1 <80182451+juno-r1@users.noreply.github.com>
2021-05-14 02:05:28 +0100

    Update anima.js

--- anima.js
+++ anima.js
@@ -8,8 +8,8 @@ function activate_quarantine() {
 
 function quarantine() {
     console.log("Transient being, knowledge without understanding bears no meaning. Continue, if you know your destination.");
-    if (window.prompt("Enter password:") === "") {
-        location.href = "index.html";
+    if (window.prompt("To reach the altar of heaven, speak the name of my temple:").toLowerCase().replace(/\s/g, '').includes("santamariainaracoeli")) {
+        location.href = "astiya-lu-nuriya.html";
     }
     else {
         console.log("403 - Forbidden");
